e6e30701b7a205fabd37e420d24253c26fbf2230
jpkoehler <102050604+jpkoehler@users.noreply.github.com>
2022-06-09 11:54:38 -0300

    Update multipage.py

--- multipage.py
+++ multipage.py
@@ -5,8 +5,6 @@ through an object oriented framework.
 
 # Import necessary libraries
 import streamlit as st
-from streamlit_option_menu import option_menu
-import streamlit.components.v1 as html
 from PIL import Image
 import numpy as np
 import io
