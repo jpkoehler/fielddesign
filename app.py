2ba69aa5579c0911c3d357b38379f7423a835e63
jpkoehler <102050604+jpkoehler@users.noreply.github.com>
2022-06-24 17:11:25 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import streamlit as st
+import utils as utl
 
 # Custom imports
 from multipage import MultiPage
@@ -10,18 +11,21 @@ app = MultiPage()
 
 # Title of the main page
 st.title("Field Design")
+utl.local_css("style.css")
 
 # Add all your applications (pages) here
 app.add_page("Propriedades do Petróleo", oilproperties.app)
 app.add_page("Reservatório e Escoamento", reservoir_flow.app)
+app.add_page("Subsea", subsea.app)
+app.add_page("Poços", wells.app)
 app.add_page("Planta de Processo - Equipamentos", equipment.app)
 app.add_page("Planta de Processo - Arranjo", layout.app)
-app.add_page("Poços", wells.app)
-app.add_page("Subsea", subsea.app)
 app.add_page("Análise de Risco", risk.app)
 app.add_page("Análise Econômica", economic.app)
 app.add_page("SMS", SMS.app)
 app.add_page("Resultados", results.app)
+
+
 
 
 # The main app
