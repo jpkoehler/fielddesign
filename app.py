f9ce21e3dafe0c8735522fae0ed30902a7fbb939
jpkoehler <102050604+jpkoehler@users.noreply.github.com>
2022-11-28 19:07:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,20 @@ from pages import reservoir_flow, oilproperties, equipment, layout, wells, subse
 app = MultiPage()
 
 # Title of the main page
-st.title("Field Design")
+# st.title("Field Design")
 utl.local_css("style.css")
 
 # Add all your applications (pages) here
-app.add_page("Características do Projeto", oilproperties.app)
-app.add_page("Reservatório e Escoamento", reservoir_flow.app)
-app.add_page("Subsea e Naval", subsea.app)
-app.add_page("Poços", wells.app)
+# app.add_page("Características do Projeto", oilproperties.app)
+# app.add_page("Reservatório e Escoamento", reservoir_flow.app)
+# app.add_page("Subsea e Naval", subsea.app)
+# app.add_page("Poços", wells.app)
 app.add_page("Planta de Processo - Equipamentos", equipment.app)
-app.add_page("Planta de Processo - Arranjo", layout.app)
-app.add_page("Análise de Risco", risk.app)
-app.add_page("Análise Econômica", economic.app)
-app.add_page("SMS", SMS.app)
-app.add_page("Resultados", results.app)
+# app.add_page("Planta de Processo - Arranjo", layout.app)
+# app.add_page("Análise de Risco", risk.app)
+# app.add_page("Análise Econômica", economic.app)
+# app.add_page("SMS", SMS.app)
+# app.add_page("Resultados", results.app)
 
 
 
